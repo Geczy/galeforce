ac9d12b1802b157cfb3f2c1a5b92273b1ace9ab9
Matt <Geczy@users.noreply.github.com>
2019-01-11 14:36:37 -0500

    Update add-ssh-key.sh

--- galeforce/data/add-ssh-key.sh
+++ galeforce/data/add-ssh-key.sh
@@ -12,23 +12,23 @@ else
   echo "ssh-rsa ENTIREKEYGOESHERE"  >> /root/.ssh/authorized_keys
 fi
 
-if awk '/PasswordAuthentication/ { print }' /etc/ssh/sshd_config; then
-	sed -i -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+if [ $(awk '/PasswordAuthentication/{var=1}END{print var+0}' /etc/ssh/sshd_config) = "1" ]; then
+  sed -i -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 else
-	echo "PasswordAuthentication doesn't exist, adding"
-	echo "PasswordAuthentication no"  >> /etc/ssh/sshd_config
+  echo "PasswordAuthentication doesn't exist, adding"
+  echo "PasswordAuthentication no"  >> /etc/ssh/sshd_config
 fi
 
-if awk '/ChallengeResponseAuthentication/ { print }' /etc/ssh/sshd_config; then
-	sed -i -e 's/ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/g' /etc/ssh/sshd_config
+if [ $(awk '/ChallengeResponseAuthentication/{var=1}END{print var+0}' /etc/ssh/sshd_config) = "1" ]; then
+  sed -i -e 's/ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/g' /etc/ssh/sshd_config
 else
-	echo "ChallengeResponseAuthentication doesn't exist, adding"
-	echo "ChallengeResponseAuthentication no"  >> /etc/ssh/sshd_config
+  echo "ChallengeResponseAuthentication doesn't exist, adding"
+  echo "ChallengeResponseAuthentication no"  >> /etc/ssh/sshd_config
 fi
 
-if awk '/UsePAM/ { print }' /etc/ssh/sshd_config; then
+if [ $(awk '/UsePAM/{var=1}END{print var+0}' /etc/ssh/sshd_config) = "1" ]; then
 	sed -i -e 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
 else
-	echo "UsePAM doesn't exist, adding"
-	echo "UsePAM no"  >> /etc/ssh/sshd_config
+  echo "UsePAM doesn't exist, adding"
+  echo "UsePAM no"  >> /etc/ssh/sshd_config
 fi
