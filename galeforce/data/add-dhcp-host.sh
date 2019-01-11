87f755fc1d67a7a532f97ddd1197e2751c3ea626
Matt <Geczy@users.noreply.github.com>
2019-01-11 14:46:03 -0500

    Update add-dhcp-host.sh

--- galeforce/data/add-dhcp-host.sh
+++ galeforce/data/add-dhcp-host.sh
@@ -3,6 +3,6 @@
 # Replace 192.168.86.xx with the IP address of your pi-hole
 # See https://github.com/marcosscriven/galeforce/issues/33
 
-sed -i -e 's/dhcp-option=${LAN_BRIDGE},6,${SETTING_BRIDGE_ADDRESS}/dhcp-option=${LAN_BRIDGE},6,192.168.86.xx/g' /etc/ssh/sshd_config
-sed -i -e 's/dhcp-option=${GUEST_BRIDGE},6,${SETTING_GUEST_BRIDGE_ADDRESS}/dhcp-option=${GUEST_BRIDGE},6,192.168.86.xx/g' /etc/ssh/sshd_config
-sed -i -e 's/dhcp-option=${SETUP_LAN_BRIDGE},6,${SETTING_SETUP_LAN_ADDRESS}/dhcp-option=${SETUP_LAN_BRIDGE},6,192.168.86.xx/g' /etc/ssh/sshd_config
+sed -i -e 's/dhcp-option=${LAN_BRIDGE},6,${SETTING_BRIDGE_ADDRESS}/dhcp-option=${LAN_BRIDGE},6,192.168.86.xx/g' /etc/init/dnsmasq.conf
+sed -i -e 's/dhcp-option=${GUEST_BRIDGE},6,${SETTING_GUEST_BRIDGE_ADDRESS}/dhcp-option=${GUEST_BRIDGE},6,192.168.86.xx/g' /etc/init/dnsmasq.conf
+sed -i -e 's/dhcp-option=${SETUP_LAN_BRIDGE},6,${SETTING_SETUP_LAN_ADDRESS}/dhcp-option=${SETUP_LAN_BRIDGE},6,192.168.86.xx/g' /etc/init/dnsmasq.conf
